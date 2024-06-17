5d997c87e860d5716693271268d3156b58df16b1
marce <colummarcelo@gmail.com>
2024-06-17 19:41:59 -0300

    add links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className="header">
-      <h1 className="header-title">Irmaos Club</h1>
-    </div>
+    <>
+      <div className="header">
+        <h1 className="header-title">Irmaos Club</h1>
+      </div>
+      <div className='link_container'>
+        <Link className="links_timer" to="/">Timer</Link>
+        <Link className="links_timer" to="/settings">Settings</Link>
+      </div>
+    </>
   );
 };
 
